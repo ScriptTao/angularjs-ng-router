30d3fefe4955e9222de7a2fabf22ed0c7cf773e5
ScriptTao <624301656@qq.com>
2019-06-28 14:15:25 +0800

    增加操作数据库方法
    增加操作数据库方法

--- view/demo/db/dbCtrl.js
+++ view/demo/db/dbCtrl.js
@@ -1,15 +1,17 @@
 monitor.controller('dbCtrl', function ($scope, $state, $stateParams, ngDialog , $interval,GetHmiParam,GetLogData,SendMessage,SendTag,GetLogData, commonFindData
 ) {
-
+    $scope.ss = 'tsw'
     commonFindData.save({
         key:"testss",
         list:[
             {
-                '@tsw':"\'tsw\'"//注意 需要用 "\'测试\'" 这种数据格式
+                //变量形式
+                '@tsw':"'"+$scope.ss+"'"
+                //死值字符串形式
+                //'@tsw':"\'tsw\'" //注意 需要用 "\'测试\'" 这种数据格式
             }
         ]
     },function (res) {
 
     })
-
 })
